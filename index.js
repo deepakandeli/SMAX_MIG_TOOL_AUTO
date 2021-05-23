58c85ecb7015a2c4f9670e0464c18a659d53eb34
Deepak Andeli <deepakandeli@gmail.com>
2021-05-23 15:44:00 +1000

    Updated Index.js with SF query logic

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const puppeteer = require('puppeteer');
+var jsforce = require('jsforce');
 var tools = require('./lib/SMAX_PS_Tools.js');
 var srcOrg = require('./lib/SMAX_PS_Src_Org.js');
 var targOrg = require('./lib/SMAX_PS_Targ_Org.js');
+var sfUtil = require('./lib/SMAX_PS_SF_Util.js');
+
 var config;
 var browser;
-
+var noOfIterations=0;
+var currIndex=0;
+var listOfComp; 
 async function main(configPath){
   //Init Config  
     //var configJSON = tools.initConfig('./config/SMAX_PS_Config.json');
@@ -12,28 +17,51 @@ async function main(configPath){
     config= JSON.parse(configJSON);
     const headLess = config.MiscSetting.HEADLESS;
     var headLessBool=false;
+    listOfComp = config.migration.components;
     if(headLess=='TRUE'){
         headLessBool=true;
     }
+
+    //If Checkonly is TRUE then it should be Headless
+    var checkOnly = config.MiscSetting.CHECKONLY;
+    if(checkOnly=='TRUE'){
+      headLessBool=false
+    }
+    
     const width=1024, height=1600;
     browser = await puppeteer.launch({
       headless: headLessBool,
       defaultViewport: null
     }); 
 
-    const listOfComp = config.migration.components;
     try{
-        for (let index = 0; index < listOfComp.length; index++) {
-            const curComp = listOfComp[index]
-            await initiateMigration(browser,config,curComp);
-          }
+      const curComp = listOfComp[currIndex];
+      await initiateMigration(browser,config,curComp);
+      await sfUtil.waitForMigComp(config.target.Username,config.target.Password,multipleCompCallBack);
     }catch(err){
         console.log(err);
-    }
+    } 
+}
 
-    finally{
-        await browser.close();
-    }    
+async function multipleCompCallBack(err, res){
+  if (err) { return console.error(err); }
+  console.log(res);
+  if(res.done && res.totalSize == 0){
+    currIndex=currIndex+1;
+    if(listOfComp.length>currIndex){
+      var curComp=listOfComp[currIndex];
+      console.log('curComp '+curComp);  
+      await initiateMigration(browser,config,curComp);
+      await tools.delay(120000);
+      await sfUtil.waitForMigComp(config.target.Username,config.target.Password,multipleCompCallBack);
+    }else{
+      await browser.close();
+    }
+  }else{
+      console.log('Job still running');
+      await tools.delay(120000);
+      await sfUtil.waitForMigComp(config.target.Username,config.target.Password,multipleCompCallBack);
+  }  
 }
 
 
@@ -45,7 +73,7 @@ async function initiateMigration(browser,config,curComp){
   const migPagePromise = page.waitForNavigation({waitUntil: "domcontentloaded"});
 
   var isSPMAlertExists = config.MiscSetting.SPM_MODAL;
-
+  
   var status='';
   await Promise.all([
     status=srcOrg.setupSrcOrg(browser,page,config)
@@ -85,42 +113,53 @@ async function initiateMigration(browser,config,curComp){
   await validateButton.click();
 
   console.log('Before Wait Response Validate');
-  const validationResponse = await page.waitForResponse(
-    (response) =>
-      response.url() === 'https://migrate.servicemax.com/MigrationTool/migration/validate' && response.status() === 200
-      ,{timeout: 130000});
+  let validateTimeout = config.MiscSetting.TIMEOUTS.VALIDATE;
+
+  try{
+    const validationResponse = await page.waitForResponse(
+        (response) =>
+          response.url() === 'https://migrate.servicemax.com/MigrationTool/migration/validate' && response.status() === 200
+          ,{timeout: validateTimeout});
+  }catch (err){
+    throw 'Validation request failed '+err;
+  }
+
   console.log('After  Wait Response Validate');
-  await tools.delay(10000);
-  console.log('After  Delay');
-  const deploySelection = page;
-  await deploySelection.$('#validateUI[style*="visibility: visible"]',{timeout: 120000});
-  console.log('After  deploySelection');
 
-  var toOverwrite = config.MiscSetting.OVERWRITE;
-  if(toOverwrite=='TRUE'){
-    //Over write
-    const selectAllOverwrite = await deploySelection.$('#selectAll',{timeout: 120000});
-    console.log('After  selectAllOverwrite');
-    await selectAllOverwrite.evaluate((e) => e.click());  
-    console.log('After  selectAllOverwrite Click');
-  } 
+  var checkOnly = config.MiscSetting.CHECKONLY;
+  if(checkOnly!='TRUE'){
+    await tools.delay(10000);
+    console.log('After  Delay');
+    const deploySelection = page;
+    await deploySelection.$('#validateUI[style*="visibility: visible"]',{timeout: 120000});
+    console.log('After  deploySelection');
   
+    var toOverwrite = config.MiscSetting.OVERWRITE;
+    if(toOverwrite=='TRUE'){
+      //Over write
+      const selectAllOverwrite = await deploySelection.$('#selectAll',{timeout: 120000});
+      console.log('After  selectAllOverwrite');
+      await selectAllOverwrite.evaluate((e) => e.click());  
+      console.log('After  selectAllOverwrite Click');
+    } 
     
-  const migrateButton = await deploySelection.waitForSelector('#deployData',{timeout: 120000});
-  console.log('After  migrateButton');
-  await migrateButton.evaluate((e) => e.click());
-  console.log('After  migrateButton Click');
-
-  //#msgBox1[style*="visibility: visible"]
-  let deployMessage = await postSelection.evaluate(() => {
-    let el = document.querySelector("#msgBox1[style*=\"visibility: visible\"]>#content");
-    return el ? el.innerText : ""
-  })
-  if(deployMessage=='None of the selected configuration items qualify for migration.'){
-    throw 'No component selected for deployment';
-  } 
-
+      
+    const migrateButton = await deploySelection.waitForSelector('#deployData',{timeout: 120000});
+    console.log('After  migrateButton');
+    await migrateButton.evaluate((e) => e.click());
+    console.log('After  migrateButton Click');
+  
+    //#msgBox1[style*="visibility: visible"]
+    let deployMessage = await postSelection.evaluate(() => {
+      let el = document.querySelector("#msgBox1[style*=\"visibility: visible\"]>#content");
+      return el ? el.innerText : ""
+    })
+    if(deployMessage=='None of the selected configuration items qualify for migration.'){
+      throw 'No component selected for deployment';
+    } 
+  }
   await page.screenshot({ path: 'example.png' });
+  await context.close();
   return ;  
 }
 
