919c34feac87c5b0041da0804ef9257832e3f519
Deepak Andeli <deepakandeli@gmail.com>
2021-06-07 20:12:01 +1000

    Updated Script

--- MigToolTest.js
+++ MigToolTest.js
@@ -1,4 +1,4 @@
-/* node MigToolTest.js "SMAX_PS_Config.json" */
+/* node MigToolTest.js "config/SMAX_PS_Config.json" > log.txt */
 const smax_migtool = require('./index.js');
 console.log(process.argv.slice(2));
 smax_migtool.main(process.argv.slice(2)[0]);
